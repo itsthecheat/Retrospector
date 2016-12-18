ad66bff3c46d0c9552dad26d5c1e420f80d31335
Nick Costanzo <nickcostanzo42@gmail.com>
2016-12-18 13:22:24 -0500

    have the sign-up sheet working, creating new branch for pass security

--- retrospeck/app/controllers/users_controller.rb
+++ retrospeck/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  #For the new form method
+  def new
+    @user = User.new
+  end
+
+  #For the post method on the new page form
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "You signed up successfully"
+      flash[:color]= "valid"
+    else
+      flash[:notice] = "Form is invalid"
+      flash[:color]= "invalid"
+    end
+    render "new"
+  end
+
 end
--- retrospeck/app/models/user.rb
+++ retrospeck/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  attr_accessor :password
+  EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  validates :user_name, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates :password, :confirmation => true #password_confirmation attr
+  validates_length_of :password, :in => 6..20, :on => :create
+
+
 end
