dde30e7af5ad49b2c873a32be791328587d3d976
Leslie Behum <llaurenb@gmail.com>
2016-12-18 15:10:59 -0500

    news api works

--- retrospeck/app/controllers/reviews_controller.rb
+++ retrospeck/app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
 
   def index
-  @response = HTTParty.get('https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=d7bd4c29053c495c9845714ee487f096')
-  @news = @response.body
+  response = HTTParty.get('https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=d7bd4c29053c495c9845714ee487f096', {format: :json})
+  @data = response['articles'][0]
   end
 end
+
