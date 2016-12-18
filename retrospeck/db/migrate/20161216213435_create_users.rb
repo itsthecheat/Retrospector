d2db6db1b36409521097acba5a23894b3f5306a5
Nick Costanzo <nickcostanzo42@gmail.com>
2016-12-18 12:16:01 -0500

    update

--- retrospeck/db/migrate/20161216213435_create_users.rb
+++ retrospeck/db/migrate/20161216213435_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :password
       t.string :email
+      t.string :encrypted_password
+      t.string :salt
 
       t.timestamps
     end
