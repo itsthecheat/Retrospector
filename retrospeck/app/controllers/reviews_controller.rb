80ec3657b9c833b27ed97d66dafa031dce58bd80
Leslie Behum <llaurenb@gmail.com>
2016-12-18 14:04:07 -0500

    test news api

--- retrospeck/app/controllers/reviews_controller.rb
+++ retrospeck/app/controllers/reviews_controller.rb
@@ -1,2 +1,7 @@
 class ReviewsController < ApplicationController
+
+  def index
+  @response = HTTParty.get('https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=d7bd4c29053c495c9845714ee487f096')
+  @news = @response.body
+  end
 end
