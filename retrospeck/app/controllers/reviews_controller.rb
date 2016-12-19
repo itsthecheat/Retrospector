8d4e17394f974844c08f7355542421335803648b
James Lee <james.lee4789@gmail.com>
2016-12-19 15:51:08 -0500

    got reviews to push to db

--- retrospeck/app/controllers/reviews_controller.rb
+++ retrospeck/app/controllers/reviews_controller.rb
@@ -1,37 +1,27 @@
 class ReviewsController < ApplicationController
     before_filter :authenticate_user, :only => [:home, :profile, :setting, :new]
+    def new
+      @reivew = Review.new
+    end
 
-    def index
-        @posts = Review.all
-      end
+    #For the post method on the new page form
+    def create
+        title = user_params[:title]
+        content = user_params[:content]
+        review_link = user_params[:review_link]
+      @review = Review.create(
+        title: title,
+        content: content,
+        review_link: review_link,
+        user_id: session[:user_id])
+        @review.save
+    
+      redirect_to "/reviews/new"
+    end
 
-      def new
-        @post = Review.new
-      end
-
-      def create
-        title = post_params[:title]
-        content = post_params[:content]
-
-        @post = Review.create(
-          title: title,
-          content: content,
-          author_id: current_admin_user.id
-        )
-
-        redirect_to post_path(@post.slug)
-      end
-
-      def edit
-        render_not_found unless @post = Review.find_by(title: params[:id])
-      end
-
-      # def update
-      #   if post = Post.find_by(slug: params[:id])
-      #     post.update(post_params)
-      #     redirect_to post_path(post_params[:slug])
-      #   else
-      #     redirect_to edit_admin_post_path
-      #   end
-      # enda
+    #Fix because this is like some hybrid version of 4/5
+    private
+    def user_params
+      params.require(:review).permit(:title, :content, :review_link)
+    end
   end
