c3ee52ff32d91d99fffcd124bfe7021425c77dff
James Lee <james.lee4789@gmail.com>
2016-12-19 16:10:12 -0500

    almost got the fucking mvp

--- retrospeck/app/controllers/reviews_controller.rb
+++ retrospeck/app/controllers/reviews_controller.rb
@@ -1,10 +1,10 @@
 class ReviewsController < ApplicationController
     before_filter :authenticate_user, :only => [:home, :profile, :setting, :new]
+    
     def new
       @reivew = Review.new
     end
 
-    #For the post method on the new page form
     def create
         title = user_params[:title]
         content = user_params[:content]
@@ -14,14 +14,14 @@ class ReviewsController < ApplicationController
         content: content,
         review_link: review_link,
         user_id: session[:user_id])
-        @review.save
-    
+      @review.save
+
       redirect_to "/reviews/new"
     end
 
-    #Fix because this is like some hybrid version of 4/5
     private
     def user_params
       params.require(:review).permit(:title, :content, :review_link)
     end
+
   end
