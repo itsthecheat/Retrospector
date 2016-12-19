5adf1582769641f37a6ea344133095cb4d751e38
Leslie Behum <llaurenb@gmail.com>
2016-12-19 15:33:55 -0500

    yelp, twitter api works

--- retrospeck/app/controllers/reviews_controller.rb
+++ retrospeck/app/controllers/reviews_controller.rb
@@ -1,15 +1,15 @@
 class ReviewsController < ApplicationController
 
-
-
   def index
+  #news_api
   response = HTTParty.get('https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=d7bd4c29053c495c9845714ee487f096', {format: :json})
   @data = response['articles'][0]
-
+  # yelp
+  params = {term: 'starbucks'}
+  res = Yelp.client.search('New York', params)
+  @yelp = res.businesses[2].snippet_text
+  #twitter
+  @tweet = $twitter.user_timeline("realdonaldtrump", count: 10)
   end
 
-
 end
-
-  # response = Yelp.client.search('San Francisco', { term: 'food' })
-  # @yelp = response.businesses[0]
--- retrospeck/config/initializers/yelp.rb
+++ retrospeck/config/initializers/yelp.rb
@@ -1,10 +1,8 @@
 require 'yelp'
 
-  client = Yelp::Client.new({
-                              consumer_key = ENV['yelp_key'],
-                              consumer_secret = ENV['yelp_secret'],
-                              token = ENV['yelp_token'],
-                              token_secret = ENV['yelp_token_secret']
-                              })
-
-
+Yelp.client.configure do |config|
+  config.consumer_key = ENV['yelp_key']
+  config.consumer_secret = ENV['yelp_secret']
+  config.token = ENV['yelp_token']
+  config.token_secret = ENV['yelp_token_secret']
+end
